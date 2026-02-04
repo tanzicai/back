4f6a734f96a45487c187a36818c919620d653bed
tanzicai <1728561744@qq.com>
2026-02-05 03:05:20 +0800

    tongbu

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/12/26/CUPS：共享打印机服务/","2025/12/30/temp/","2025/12/26/工作分配的一些思考/","2026/01/07/高斯泼溅技术/","2025/12/26/pages/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
---
+++ source/shuoshuo/index.md
@@ -0,0 +1,8 @@
+---
+title: 小碎片
+date: 2018-06-07 22:17:49
+type: 'shuoshuo'
+---
+limit:
+  type: date
+  value: 2024-06-20
